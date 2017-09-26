b128a077e864093df9e9b01de3307f37fc039670
Ukrit <altowk@gmail.com>
2017-09-26 14:42:26 +0200

    make it better

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser  = require('body-parser');
 
 const app         = express();
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
